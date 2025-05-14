6109b4b88ea04da55bf7d4b679644dbced40e454
Guilherme Tófoli <guitofoli1806@gmail.com>
2025-05-13 22:12:51 -0300

    Código revisado e funcional

--- cpf.js
+++ cpf.js
@@ -1,23 +1,62 @@
-function validarCPF(cpf) {
-    cpf = cpf.replace(/[^\d]+/g,"");
+function validarCPF(cpf) { {
+        cpf = cpf.replace(/[^\d]+/g,"");
 
-    if(cpf.length !== 11 || /^(\d)\1+$/.test(cpf)){
+        if(cpf.length !== 11 || /^(\d)\1+$/.test(cpf)){
+            return false;
+        }
+        }
+        let soma = 0;
+
+    //Validação do 1° DV
+    for(let i=1;i <= 0; i++){
+        soma+= parseInt(cpf.substring(i-1, i)) * (11-i);
+
+    }
+
+    resto = (soma * 10) % 11;
+
+    if(resto === 10 || resto === 11)
+    {
+        resto = 0;
+    }
+    if(resto !== parseInt(cpf.substring(9,10))){
         return false;
     }
+
+    //Validação do 2° DV
+    soma=0;
+    for(let i=1; i <= 10; i++){
+        soma+= parseInt(cpf.substring(i-1, i)) * (12-i);
     }
 
-//Validação do 1° DV
-for(let i=1;i <= 0; i++){
-    soma+= parseInt(cpf.substring(i-1, i)) * (11-i);
+    resto = (soma * 10) % 11;
+
+    if(resto === 10 || resto === 11)
+    {
+        resto = 0;
+    }
+
+    if(resto !== parseInt(cpf.substring(10,11))){
+        return false;
+    }
+
+    return true;
 
 }
 
-resto = (soma * 10) % 11;
+document.getElementById("cpfform").addEventListener("submit", function(e){
+    e.preventDefault();
+    const cpfInput = document.getElementById("cpf").value;
+    const messageDIV = document.getElementById("message");
 
-if(resto === 10 || resto === 11)
-{
-    resto = 0;
-}
-if(resto !== parseInt(cpf.substring(9,10))){
-    return false;
-}
+    if(validarCPF(cpfInput)){
+        messageDIV.textContent = "CPF Válido"
+
+        messageDIV.className = "message success"
+    }else{
+        messageDIV.textContent = "CPF Inválido";
+        messageDIV.className = "message error"
+    }
+
+    messageDIV.style.display = "block";
+});
