d2873c6c103c1a33095a9b6bf461258002970007
Guilherme Tófoli <guitofoli1806@gmail.com>
2025-05-13 21:33:27 -0300

    Adicionado validação do 1° DV

--- cpf.js
+++ cpf.js
@@ -5,3 +5,16 @@ function validarCPF(cpf) {
         return false;
     }
     }
+
+//Validação do 1° DV
+for(let i=1;i <= 0; i++){
+    soma+= parseInt(cpf.substring(i-1, i)) * (11-i);
+
+}
+
+resto = (soma * 10) % 11;
+
+if(resto === 10 || resto === 11)
+{
+    resto = 0;
+}
